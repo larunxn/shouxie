66599ce0100f5fcf1cbd841975770cbbe2d73f5e
larunxn <1913098455@qq.com>
2023-11-19 20:25:46 +0800

    first commit

--- Minst_多分类_softmax/mnist数据集.py
+++ Minst_多分类_softmax/mnist数据集.py
@@ -2,3 +2,26 @@
 # @Time: 2023/11/19 20:24
 # @Author: Changmeng Yang
 
+import numpy as np
+import os
+import struct
+import matplotlib.pyplot as plt
+
+
+def load_images(file):  # 加载数据
+    with open(file, "rb") as f:
+        data = f.read()
+    magic_number, num_items, rows, cols = struct.unpack(">iiii", data[:16])
+    return np.asanyarray(bytearray(data[16:]), dtype=np.uint8).reshape(num_items, 28,28)
+
+
+def load_labels(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return np.asanyarray(bytearray(data[8:]), dtype=np.int32)
+
+
+if __name__ == "__main__":
+    train_images = load_images(os.path.join("..","data","mnist","train-images.idx3-ubyte"))
+    train_labels = load_labels(os.path.join("..","data","mnist","train-labels.idx1-ubyte"))
+    print("")
