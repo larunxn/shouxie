a6f47c75e0c33bddcdc1542a9271a01fb06abd44
larunxn <1913098455@qq.com>
2023-12-06 16:53:33 +0800

    加入sigmoid、softmax

--- Minst_多分类_softmax/手写数字识别12.py
+++ Minst_多分类_softmax/手写数字识别12.py
@@ -8,15 +8,13 @@ import numpy as np
 import os
 import struct
 import matplotlib.pyplot as plt
-import torch.nn as nn
 
 
 def load_images(file):  # 加载数据
     with open(file, "rb") as f:
         data = f.read()
     magic_number, num_items, rows, cols = struct.unpack(">iiii", data[:16])
-    return np.asanyarray(bytearray(data[16:]), dtype=np.uint8).reshape(num_items, 28, 28)
-
+    return np.asanyarray(bytearray(data[16:]), dtype=np.uint8).reshape(num_items, 28,28)
 
 def load_labels(file):
     with open(file, "rb") as f:
@@ -24,16 +22,16 @@ def load_labels(file):
     return np.asanyarray(bytearray(data[8:]), dtype=np.int32)
 
 
-def make_onehot(labels, class_num):
-    result = np.zeros((labels.shape[0], class_num))
+def make_onehot(labels,class_num):
+    result = np.zeros((labels.shape[0],class_num))
 
-    for idx, cls in enumerate(labels):
+    for idx,cls in enumerate(labels):
         result[idx][cls] = 1
     return result
 
 
 class Dataset:
-    def __init__(self, all_images, all_labels):
+    def __init__(self,all_images,all_labels):
         self.all_images = all_images
         self.all_labels = all_labels
 
@@ -41,14 +39,14 @@ class Dataset:
         image = self.all_images[index]
         label = self.all_labels[index]
 
-        return image, label
+        return image,label
 
     def __len__(self):
         return len(self.all_images)
 
 
 class DataLoader:
-    def __init__(self, dataset, batch_size, shuffle=True):
+    def __init__(self,dataset,batch_size,shuffle=True):
         self.dataset = dataset
         self.batch_size = batch_size
         self.shuffle = shuffle
@@ -73,25 +71,23 @@ class DataLoader:
             batch_labels.append(data[1])
 
             self.cursor += 1
-        return np.array(batch_imges), np.array(batch_labels)
+        return np.array(batch_imges),np.array(batch_labels)
 
 
 def softmax(x):
-    x = np.clip(x, -1e10, 100)
+    x = np.clip(x,-1e10,100)
     ex = np.exp(x)
-    sum_ex = np.sum(ex, axis=1, keepdims=True)
+    sum_ex = np.sum(ex,axis=1,keepdims=True)
 
-    result = ex / sum_ex
+    result = ex/sum_ex
 
-    result = np.clip(result, 1e-10, 1e10)
-    return result
-
+    result = np.clip(result,1e-10,1e10)
+    return  result
 
 def sigmoid(x):
-    x = np.clip(x, -100, 1e10)
-    result = 1 / (1 + np.exp(-x))
+    x = np.clip(x,-100,1e10)
+    result = 1/(1 + np.exp(-x))
     return result
-
 
 class Linear:
     def __init__(self, in_features, out_features):
@@ -100,13 +96,12 @@ class Linear:
 
     def forward(self, x):
         result = x @ self.weight + self.bias
-
         self.x = x
         return result
 
-    def backward(self, G):
+    def backward(self,G):
         delta_w = self.x.T @ G
-        delta_b = np.sum(G, axis=0)
+        delta_b = np.sum(G,axis=0)
 
         self.weight -= lr * delta_w
         self.bias -= lr * delta_b
@@ -141,60 +136,63 @@ class Softmax:
 
         return G
 
+class ModuleList:
+    def __init__(self,layers):
+        self.layers = layers
+
+    def forward(self,x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def backward(self,G):
+        for layer in self.layers[::-1]:
+            G = layer.backward(G)
+
 
 if __name__ == "__main__":
-    train_images = load_images(os.path.join("..", "data", "mnist", "train-images.idx3-ubyte")) / 255
-    train_labels = make_onehot(load_labels(os.path.join("..", "data", "mnist", "train-labels.idx1-ubyte")), 10)
+    train_images = load_images(os.path.join("..","data","mnist","train-images.idx3-ubyte"))/255
+    train_labels = make_onehot(load_labels(os.path.join("..","data","mnist","train-labels.idx1-ubyte")),10)
 
     dev_images = load_images(os.path.join("..", "data", "mnist", "t10k-images.idx3-ubyte")) / 255
     dev_labels = load_labels(os.path.join("..", "data", "mnist", "t10k-labels.idx1-ubyte"))
 
-    train_images = train_images.reshape(60000, 784)
-    dev_images = dev_images.reshape(-1, 784)
+    train_images = train_images.reshape(60000,784)
+    dev_images = dev_images.reshape(-1,784)
 
     batch_size = 50
     shuffle = False
 
-    train_dataset = Dataset(train_images, train_labels)
-    train_dataloader = DataLoader(train_dataset, batch_size, shuffle)
+    train_dataset = Dataset(train_images,train_labels)
+    train_dataloader = DataLoader(train_dataset,batch_size,shuffle)
 
     dev_dataset = Dataset(dev_images, dev_labels)
     dev_dataloader = DataLoader(dev_dataset, batch_size, shuffle)
 
-    linear1_layer = Linear(784, 256)
-    sigmoid_layer = Sigmoid()
-    linear2_layer = Linear(256, 300)
-    linear3_layer = Linear(300, 10)
-    softmax_layer = Softmax()
+    model = ModuleList([
+        Linear(784, 256),
+        Linear(256, 10),
+        Softmax()
+    ])
 
     epoch = 100
-    lr = 0.0003
+    lr = 0.0001
 
     for e in range(epoch):
-        for x, batch_labels in train_dataloader:
-            x = linear1_layer.forward(x)
-            x = sigmoid_layer.forward(x)
-            x = linear2_layer.forward(x)
-            x = linear3_layer.forward(x)
+        for x, l in train_dataloader:
+            x = model.forward(x)
 
-            p = softmax_layer.forward(x)
+            loss = - np.mean(l * np.log(x))
+            model.backward(l)
 
-            loss = - np.mean(batch_labels * np.log(p))
 
-            G = softmax_layer.backward(batch_labels)
-            G = linear3_layer.backward(G)
-            G = linear2_layer.backward(G)
-            G = sigmoid_layer.backward(G)
-            G = linear1_layer.backward(G)
 
         right = 0
-        for x, batch_labels in dev_dataloader:
-            x = linear1_layer.forward(x)
-            x = sigmoid_layer.forward(x)
-            x = linear2_layer.forward(x)
-            x = linear3_layer.forward(x)
-            pre_idx = np.argmax(x, axis=-1)
+        for x,batch_labels in dev_dataloader:
+            x = model.forward(x)         # 第四层
+
+            pre_idx = np.argmax(x, axis = -1)
 
             right += np.sum(pre_idx == batch_labels)
-        acc = right / len(dev_dataset)
+        acc = right/len(dev_dataset)
         print(f"acc:{acc:.3f}")
